1acbf053d1d14df90c68a2fe3e3fbab54974f42a
Jraaf <nasibovrafael1@gmail.com>
2023-03-14 22:07:18 +0200

    lab3

---
+++ Lab3/Lab3/Counter.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.Remoting.Messaging;
+using System.Text.Json.Serialization;
+
+namespace Lab3
+{
+    internal class Counter
+    {
+        public double Value { get; set; }
+
+        public double LowerBorder { get; set; } 
+
+        public double UpperBorder { get; set; }
+
+        public Counter() { }
+        private Counter(double value) : this(value, value - 1, value + 1)
+        {
+            Value = value;
+        }
+        private Counter(double value, double lowerBorder, double upperBorder)
+        {
+            Value = value;
+            LowerBorder = lowerBorder;
+            UpperBorder = upperBorder;
+        }
+        //public Counter() : this(0)
+        //{ }
+
+        //public Counter(double value) : this(value - 1, value + 1)
+        //{
+        //    Value = value;
+        //}
+        //public Counter(double lowerBorder, double upperBorder) : this((lowerBorder + upperBorder) / 2, lowerBorder, upperBorder)
+        //{
+        //    LowerBorder = lowerBorder;
+        //    UpperBorder = upperBorder;
+        //}
+        //[JsonConstructor]
+        //public Counter(double value, double lowerBorder, double upperBorder)
+        //{
+        //    Value = value;
+        //    LowerBorder = lowerBorder;
+        //    UpperBorder = upperBorder;
+        //}
+
+        public override string ToString()
+        {
+            return $"Value: {Value}\nLowerBorder: {LowerBorder}\nUpperBorder: {UpperBorder}";
+        }
+
+        public Counter Clone()
+        {
+            return new Counter(Value,LowerBorder,UpperBorder);
+        }
+
+        #region [Operators]
+        public static Counter operator ++(Counter a)
+        {
+            if (a.Value +1 > a.UpperBorder) throw new ArgumentOutOfRangeException("UpperBound reached");
+            return new Counter()
+            {
+                Value=a.Value+1,
+                LowerBorder=a.LowerBorder,
+                UpperBorder=a.UpperBorder
+            };
+        }
+
+        public static Counter operator --(Counter a)
+        {
+            if (a.Value-1 < a.LowerBorder) throw new ArgumentOutOfRangeException("LowerBound Reached");
+            return new Counter()
+            {
+                Value = a.Value - 1,
+                LowerBorder = a.LowerBorder,
+                UpperBorder = a.UpperBorder
+            };
+        }
+        public static Counter operator +(Counter a, double b)
+        {
+            if (a.Value + b > a.UpperBorder) throw new ArgumentOutOfRangeException("UpperBound reached");
+            return new Counter()
+            {
+                Value = a.Value + 1,
+                LowerBorder = a.LowerBorder,
+                UpperBorder = a.UpperBorder
+            };
+        }
+        public static Counter operator -(Counter a, double b)
+        {
+            if (a.Value - b < a.LowerBorder) throw new ArgumentOutOfRangeException("LowerBound Reached");
+            return new Counter()
+            {
+                Value = a.Value - b,
+                LowerBorder = a.LowerBorder,
+                UpperBorder = a.UpperBorder
+            };
+        }
+        public static Counter operator *(Counter a, double b)
+        {
+            if (a.Value * b > a.UpperBorder) throw new ArgumentOutOfRangeException("UpperBound reached");
+            return new Counter()
+            {
+                Value = a.Value * b,
+                LowerBorder = a.LowerBorder,
+                UpperBorder = a.UpperBorder
+            };
+        }
+        public static Counter operator /(Counter a, double b)
+        {
+            if (a.Value / b < a.LowerBorder) throw new ArgumentOutOfRangeException("LowerBound Reached");
+            return new Counter()
+            {
+                Value = a.Value / b,
+                LowerBorder = a.LowerBorder,
+                UpperBorder = a.UpperBorder
+            };
+        }
+        public static bool operator <(Counter a, double b) => a.Value < b;
+        public static bool operator >(Counter a, double b) => a.Value > b;
+        #endregion
+
+
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+using System.CodeDom;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static string Path = @"C:\\Users\\nasib\\Desktop\\Projects\\ProgLabs\\Lab3\\Lab3\\data.json";
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+
+
+            Counter count = new Counter { Value = 9,LowerBorder=7,UpperBorder=10 };
+
+            Console.WriteLine("");
+            Console.WriteLine(count);
+
+
+
+            #region [Testing of a class]
+            Console.WriteLine("\t\tTesting of a class");
+            Console.WriteLine("\n\t5x c++");
+            var c = count.Clone();
+            Testing(c, (o) =>
+            {
+                for (int i = 0; i < 5; i++) 
+                    c++;
+            });
+            Console.WriteLine("\n\t5x c--");
+            Testing(c, (o) =>
+            {
+                for (int i = 0; i < 5; i++)
+                    c--;
+            });
+            Console.WriteLine("\n\t*=5");
+            Testing(c, (o) => o *= 5);
+            Console.WriteLine("\n\t/=5");
+            Testing(c, (o) => o /= 5);
+            Console.WriteLine("\n\t+=5");
+            Testing(c, (o) => o += 5);
+            Console.WriteLine("\n\t-=5");
+            Testing(c, (o) => o -= 5);
+            #endregion
+
+            #region [Serialization]
+            SerializeObj(count, Path);
+
+            var counter = DeserializeCounter(Path);
+            #endregion
+
+            Console.WriteLine("\n\t\tBefore Serialization\n"+count);
+            Console.WriteLine("\n\t\tAfter Deserialization\n"+counter);
+            Console.ReadLine();
+        }
+        static void SerializeObj(object obj,string path)
+        {
+            var cJson = JsonConvert.SerializeObject(obj,Formatting.Indented);
+            File.WriteAllText(path, cJson);
+        }
+        static Counter DeserializeCounter(string path)
+        {
+            var objStr = File.ReadAllText(Path);
+
+            return JsonConvert.DeserializeObject<Counter>(objStr);
+        }
+        static void Testing(Counter counter,Action<Counter> f)
+        {
+            Counter c = counter.Clone();
+            Console.WriteLine();
+            try
+            {
+                f(c);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+    }
+}
